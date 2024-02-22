9704c4dc6004654bbdc9afbc47fa5e3d7094ff01
Avinash Chinnala <143155028+AvinashChinnala1308@users.noreply.github.com>
2024-02-22 02:00:29 -0600

    Added Lottie Animation for the app

--- PET MART/PET MART/ViewController.swift
+++ PET MART/PET MART/ViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import Lottie
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var LaunchLAV: LottieAnimationView!
+    {
+        didSet{
+            LaunchLAV.loopMode = .playOnce
+            LaunchLAV.animationSpeed = 1.0
+            LaunchLAV.play { [weak self] _ in
+                UIViewPropertyAnimator.runningPropertyAnimator(
+                    withDuration: 1.0,
+                    delay: 0.0,
+                    options:[.curveEaseInOut]){
+                        self?.LaunchLAV.alpha = 0.0
+                    }
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    
 }
 
