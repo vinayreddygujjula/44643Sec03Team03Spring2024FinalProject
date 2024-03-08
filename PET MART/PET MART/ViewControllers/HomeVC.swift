3d978dce7ebdf4061754cda47c78fc78200d2d23
Vinay Reddy Gujjula <vinayreddyg02@gmail.com>
2024-03-07 19:08:39 -0600

    added home view controller coca touch class

---
+++ PET MART/PET MART/ViewControllers/HomeVC.swift
@@ -0,0 +1,30 @@
+//
+//  HomeVC.swift
+//  PET MART
+//
+//  Created by Vinay Reddy Gujjula on 3/7/24.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        self.navigationItem.setHidesBackButton(true, animated: true)
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
