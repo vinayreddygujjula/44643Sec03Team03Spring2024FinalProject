3f9ddfb9e975574437550a26e3ff711198273484
Hema Likhitha Adapa <s565725@nwmissouri.edu>
2024-03-07 23:04:17 -0600

    Outlets created

---
+++ PET MART/PET MART/ViewControllers/SignUpVC.swift
@@ -0,0 +1,46 @@
+//
+//  SignUpVC.swift
+//  PET MART
+//
+//  Created by Hema Likhitha Adapa on 3/7/24.
+//
+
+import UIKit
+
+class SignUpVC: UIViewController {
+
+    @IBOutlet weak var FirstNameTF: UITextField!
+    
+    @IBOutlet weak var LastnameTF: UITextField!
+    
+    @IBOutlet weak var EmailTF: UITextField!
+    
+    
+    @IBOutlet weak var PasswordTF: UITextField!
+    
+    
+    
+    @IBOutlet weak var ConfirmpasswordTF: UITextField!
+    
+    
+    @IBOutlet weak var SignUpBTN: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
