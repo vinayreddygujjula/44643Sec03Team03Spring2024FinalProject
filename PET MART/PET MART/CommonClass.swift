1e9aa156d59823ae4d2c8d1d1710e032c24f566a
Vinay Reddy Gujjula <vinayreddyg02@gmail.com>
2024-03-09 13:21:42 -0600

    added login and signup validations for text fields

---
+++ PET MART/PET MART/CommonClass.swift
@@ -0,0 +1,25 @@
+//
+//  CommonClass.swift
+//  PET MART
+//
+//  Created by Vinay Reddy Gujjula on 3/9/24.
+//
+
+import Foundation
+import UIKit
+
+class Common{
+    
+    static func isValidEmail(_ email: String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        
+        let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailPred.evaluate(with: email)
+    }
+    
+    static func applyBorderProperties(to element: UIView){
+        element.layer.cornerRadius = 5.0
+        element.layer.borderWidth = 1.0
+        element.layer.borderColor = UIColor.black.cgColor
+    }
+}
--- PET MART/PET MART/ViewControllers/SignUpVC.swift
+++ PET MART/PET MART/ViewControllers/SignUpVC.swift
@@ -10,26 +10,72 @@ import UIKit
 class SignUpVC: UIViewController {
 
     @IBOutlet weak var FirstNameTF: UITextField!
-    
     @IBOutlet weak var LastnameTF: UITextField!
-    
     @IBOutlet weak var EmailTF: UITextField!
-    
-    
     @IBOutlet weak var PasswordTF: UITextField!
-    
-    
-    
     @IBOutlet weak var ConfirmpasswordTF: UITextField!
-    
-    
     @IBOutlet weak var SignUpBTN: UIButton!
     
+    @IBAction func signUpAction(_ sender: UIButton) {
+        
+        guard let firstName = self.FirstNameTF.text, !firstName.isEmpty
+        else{
+            self.FirstNameTF.layer.borderColor = UIColor.red.cgColor
+            return
+        }
+        self.FirstNameTF.layer.borderColor = UIColor.black.cgColor
+        
+        guard let lastName = self.LastnameTF.text, !lastName.isEmpty
+        else{
+            self.LastnameTF.layer.borderColor = UIColor.red.cgColor
+            return
+        }
+        self.LastnameTF.layer.borderColor = UIColor.black.cgColor
+        
+        guard let email = self.EmailTF.text, !email.isEmpty, Common.isValidEmail(email)
+        else{
+            self.EmailTF.layer.borderColor = UIColor.red.cgColor
+            return
+        }
+        self.EmailTF.layer.borderColor = UIColor.black.cgColor
+        
+        guard let password = self.PasswordTF.text, !password.isEmpty
+        else{
+            self.PasswordTF.layer.borderColor = UIColor.red.cgColor
+            return
+        }
+        self.PasswordTF.layer.borderColor = UIColor.black.cgColor
+        
+        guard let cnfrmPassword = self.ConfirmpasswordTF.text, !cnfrmPassword.isEmpty, cnfrmPassword == password
+        else{
+            self.ConfirmpasswordTF.layer.borderColor = UIColor.red.cgColor
+            return
+        }
+        self.ConfirmpasswordTF.layer.borderColor = UIColor.black.cgColor
+        
+        self.performSegue(withIdentifier: "home", sender: sender)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        switch(segue.identifier){
+        case "home" :
+            guard let destinationVC = segue.destination as? HomeVC
+            else{
+                return
+            }
+        default :
+            break
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        Common.applyBorderProperties(to: FirstNameTF)
+        Common.applyBorderProperties(to: LastnameTF)
+        Common.applyBorderProperties(to: EmailTF)
+        Common.applyBorderProperties(to: PasswordTF)
+        Common.applyBorderProperties(to: ConfirmpasswordTF)
     }
     
 
