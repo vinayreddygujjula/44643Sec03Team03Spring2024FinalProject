ba4b96062e2824fc7a4451d484bf360e13d37b60
s564568 <s564568@nwmissouri.edu>
2024-03-29 21:03:39 -0500

    added pet description view control

---
+++ PET MART/PET MART/PetControlDescriptionPageVC.swift
@@ -0,0 +1,29 @@
+//
+//  PetControlDescriptionPageVC.swift
+//  PET MART
+//
+//  Created by Lakshmi Sai Teja Padam on 3/29/24.
+//
+
+import UIKit
+
+class PetControlDescriptionPageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
