c4d138657bf5205517691e4d9094719e5e23f2b2
Hema Likhitha Adapa <s565725@nwmissouri.edu>
2024-03-29 19:52:32 -0500

    added constraints

---
+++ PET MART/PET MART/ViewControllers/ProductsVC.swift
@@ -0,0 +1,29 @@
+//
+//  ProductsVC.swift
+//  PET MART
+//
+//  Created by Hema Likhitha Adapa on 3/29/24.
+//
+
+import UIKit
+
+class ProductsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
