e57594413913ee1bac96767f063b6ea413499e80
Avinash Chinnala <143155028+AvinashChinnala1308@users.noreply.github.com>
2024-04-05 15:19:12 -0500

    created view controllers for pets and products

---
+++ PET MART/PET MART/ViewControllers/CartVC.swift
@@ -0,0 +1,34 @@
+//
+//  CartVC.swift
+//  PET MART
+//
+//  Created by Avinash Chinnala  on 4/5/24.
+//
+
+import UIKit
+
+class CartVC: UIViewController{
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       
+    }
+    @IBOutlet weak var productsTV: UITableView!
+    @IBOutlet weak var priceLBL: UILabel!
+    @IBOutlet weak var messageLBL: UILabel!
+
+    @IBAction func buyCart(_ sender: UIButton) {
+    }
+    
+    
+    
+    @IBAction func clearCart(_ sender: UIButton) {
+    }
+    
+    
+    
+    
+    
+
+}
---
+++ PET MART/PET MART/ViewControllers/ProductsVC.swift
@@ -0,0 +1,22 @@
+//
+//  ProductsVC.swift
+//  PET MART
+//
+//  Created by Avinash Chinnala  on 4/5/24.
+//
+
+import UIKit
+
+class ProductsVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+   
+
+    
+
+ 
+
+}
