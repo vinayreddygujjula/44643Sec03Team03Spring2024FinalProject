fe6ad8ddb076ae8b32186be7cd2d304e5440ed81
s564568 <s564568@nwmissouri.edu>
2024-04-12 20:45:19 -0500

    ADDED FUNCTONALITY

---
+++ PET MART/PET MART/findVC.swift
@@ -0,0 +1,37 @@
+//
+//  findVC.swift
+//  PET MART
+//
+//  Created by Lakshmi Sai Teja Padam on 4/12/24.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pets = ["Dog", "Cat", "Rabbit", "Parrot"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pets.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PetCell", for: indexPath)
+        cell.textLabel?.text = pets[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "showDetail", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    }
+}
