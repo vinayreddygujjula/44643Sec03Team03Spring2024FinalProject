4b2e8f2c4a7da632c03f2aebe83b9c1d56089fea
s564568 <s564568@nwmissouri.edu>
2024-04-12 22:54:27 -0500

    added add pet functonality

---
+++ PET MART/PET MART/AddPetViewController.swift
@@ -0,0 +1,31 @@
+//
+//  AddPetViewController.swift
+//  PET MART
+//
+//  Created by Lakshmi Sai Teja Padam on 4/12/24.
+//
+
+import UIKit
+
+class AddPetViewController: UIViewController {
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var descriptionTextField: UITextField!
+    @IBOutlet weak var priceTextField: UITextField!
+    @IBOutlet weak var addButton: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Setup any additional setup after loading the view.
+    }
+
+    @IBAction func addPetButtonTapped(_ sender: UIButton) {
+        guard let name = nameTextField.text, !name.isEmpty,
+              let description = descriptionTextField.text, !description.isEmpty,
+              let priceText = priceTextField.text, let price = Double(priceText) else {
+            
+            return
+        }
+        
+        let newPet = Pet(name: name, description: description, price: price, imageName: "default")
+    }
+}
